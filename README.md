65dd4f013ddf2759949cbb48f3c58987415d6c5f
PhoebeNan <13037650105@163.com>
2019-10-15 13:53:15 +0800

    add bootstrap and modifity md file

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 ## 码楠笔记
 
 ## 资料网站
-    https://github.com/PhoebeNan/community
-     https://spring.io/guides
-'
+[github仓库](https://github.com/PhoebeNan/community)
+[spring web](https://spring.io/guides)
+[github登录接入](https://developer.github.com/apps/)
+[bootstrap文档](https://v3.bootcss.com/)
+
+
 ## 工具
---
+++ src/main/java/life/majiang/community/controller/IndexController.java
@@ -0,0 +1,33 @@
+package life.majiang.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * @author zhaoyanan
+ * @create 2019-10-14-9:33
+ */
+@Controller
+public class IndexController {
+
+    /**
+     * 测试Thymeleaf模板引擎
+     * @param
+     * @return
+     */
+//    @GetMapping("/index")
+//    public String greeting(@RequestParam(name = "name",required = false,defaultValue = "World")
+//                           String namex, Model model){
+//        model.addAttribute("name",namex);
+//        return "index";
+//    }
+
+
+    @GetMapping("/")
+    public String index(){
+
+        return "index";
+    }
+}
