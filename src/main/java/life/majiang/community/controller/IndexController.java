40c683e83603ea52c279c142aec8c2e69aa77c1d
PhoebeNan <13037650105@163.com>
2019-10-22 09:24:55 +0800

    index

--- src/main/java/life/majiang/community/controller/IndexController.java
+++ src/main/java/life/majiang/community/controller/IndexController.java
@@ -2,12 +2,9 @@ package life.majiang.community.controller;
 
 import life.majiang.community.mapper.UserMapper;
 import life.majiang.community.model.User;
-import net.bytebuddy.asm.Advice;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
